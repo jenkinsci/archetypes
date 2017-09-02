aef9a1f9fd9eed8d125c59792c9d0e3af3b54ec5
rsandell <rsandell@cloudbees.com>
2017-09-02 05:54:10 +0200

    The traditional but updated hello world builder

---
+++ hello-world/src/main/resources/archetype-resources/src/test/java/HelloWorldBuilderTest.java
@@ -0,0 +1,78 @@
+package $package;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class HelloWorldBuilderTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    final String name = "Bobby";
+
+    @Test
+    public void testConfigRoundtrip() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        project.getBuildersList().add(new HelloWorldBuilder(name));
+        project = jenkins.configRoundtrip(project);
+        jenkins.assertEqualDataBoundBeans(new HelloWorldBuilder(name), project.getBuildersList().get(0));
+    }
+
+    @Test
+    public void testConfigRoundtripFrench() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        HelloWorldBuilder builder = new HelloWorldBuilder(name);
+        builder.setUseFrench(true);
+        project.getBuildersList().add(builder);
+        project = jenkins.configRoundtrip(project);
+
+        HelloWorldBuilder lhs = new HelloWorldBuilder(name);
+        lhs.setUseFrench(true);
+        jenkins.assertEqualDataBoundBeans(lhs, project.getBuildersList().get(0));
+    }
+
+    @Test
+    public void testBuild() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        HelloWorldBuilder builder = new HelloWorldBuilder(name);
+        project.getBuildersList().add(builder);
+
+        FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
+        jenkins.assertLogContains("Hello, " + name, build);
+    }
+
+    @Test
+    public void testBuildFrench() throws Exception {
+
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        HelloWorldBuilder builder = new HelloWorldBuilder(name);
+        builder.setUseFrench(true);
+        project.getBuildersList().add(builder);
+
+        FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
+        jenkins.assertLogContains("Bonjour, " + name, build);
+    }
+
+    @Test
+    public void testScriptedPipeline() throws Exception {
+        String agentLabel = "my-agent";
+        jenkins.createOnlineSlave(Label.get(agentLabel));
+        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
+        String pipelineScript
+                = "node {\n"
+                + "  greet '" + name + "'\n"
+                + "}";
+        job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+        WorkflowRun completedBuild = jenkins.assertBuildStatusSuccess(job.scheduleBuild2(0));
+        String expectedString = "Hello, " + name + "!";
+        jenkins.assertLogContains(expectedString, completedBuild);
+    }
+
+}
