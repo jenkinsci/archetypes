c94135b77fca72f37089f2878a328f1b8af7771d
Steven Christou <schristou88@gmail.com>
2017-09-02 17:42:53 -0700

    Provide more methods which need overriding.

--- scm-plugin/src/main/resources/archetype-resources/src/main/java/HelloWorldSCM.java
+++ scm-plugin/src/main/resources/archetype-resources/src/main/java/HelloWorldSCM.java
@@ -6,26 +6,38 @@ import java.io.IOException;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.scm.ChangeLogParser;
+import hudson.scm.PollingResult;
 import hudson.scm.SCM;
 import hudson.scm.SCMDescriptor;
 import hudson.scm.SCMRevisionState;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.SCMRevision;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.io.File;
-import java.io.IOException;
 
 public class HelloWorldSCM extends SCM {
+    HelloWorldSCM(SCMHead head, SCMRevision revision) {
+
+    }
+
     @Override
     public ChangeLogParser createChangeLogParser() {
         return new HelloWorldSCMChangeLogParser();
     }
 
     @Override
+    public PollingResult compareRemoteRevisionWith(@Nonnull Job<?, ?> project, @Nullable Launcher launcher, @Nullable FilePath workspace, @Nonnull TaskListener listener, @Nonnull SCMRevisionState baseline) throws IOException, InterruptedException {
+        return null;
+    }
+
+    @Override
     public void checkout(@Nonnull Run<?, ?> build, @Nonnull Launcher launcher, @Nonnull FilePath workspace, @Nonnull TaskListener listener, File changelogFile, SCMRevisionState baseline) throws IOException, InterruptedException {
+        // Perform the actual checkout operation (download data to workspace).
     }
 
 
@@ -38,8 +50,7 @@ public class HelloWorldSCM extends SCM {
     public static class DescriptorImpl extends SCMDescriptor<HelloWorldSCM> {
 
         public DescriptorImpl() {
-            super(HelloWorldSCM.class, null);
-            load();
+            super(null);
         }
 
         @Override
