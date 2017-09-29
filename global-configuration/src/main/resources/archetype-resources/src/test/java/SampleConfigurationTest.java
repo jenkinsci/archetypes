0622ebbde2d6e1ba6da7d2413573e1c5b7965583
Jesse Glick <jglick@cloudbees.com>
2017-09-29 16:29:25 -0400

    Added archetype for global configuration.

---
+++ global-configuration/src/main/resources/archetype-resources/src/test/java/SampleConfigurationTest.java
@@ -0,0 +1,30 @@
+package $package;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.jvnet.hudson.test.RestartableJenkinsRule;
+
+public class SampleConfigurationTest {
+
+    @Rule
+    public RestartableJenkinsRule rr = new RestartableJenkinsRule();
+
+    @Test
+    public void uiAndStorage() {
+        rr.then(r -> {
+            assertNull("not set initially", SampleConfiguration.get().getLabel());
+            HtmlForm config = r.createWebClient().goTo("configure").getFormByName("config");
+            HtmlTextInput textbox = config.getInputByName("_.label");
+            textbox.setText("hello");
+            r.submit(config);
+            assertEquals("global config page let us edit it", "hello", SampleConfiguration.get().getLabel());
+        });
+        rr.then(r -> {
+            assertEquals("still there after restart of Jenkins", "hello", SampleConfiguration.get().getLabel());
+        });
+    }
+
+}
