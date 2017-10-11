e9a81a0c5b59b3aa96cd3c3f37c0bdca919c41a3
Jesse Glick <jglick@cloudbees.com>
2017-10-11 18:09:53 -0400

    Adding explanatory comments to test.

--- global-configuration/src/main/resources/archetype-resources/src/test/java/SampleConfigurationTest.java
+++ global-configuration/src/main/resources/archetype-resources/src/test/java/SampleConfigurationTest.java
@@ -12,6 +12,15 @@ public class SampleConfigurationTest {
     @Rule
     public RestartableJenkinsRule rr = new RestartableJenkinsRule();
 
+    /**
+     * Tries to exercise enough code paths to catch common mistakes:
+     * <ul>
+     * <li>missing {@code load}
+     * <li>missing {@code save}
+     * <li>misnamed or absent getter/setter
+     * <li>misnamed {@code textbox}
+     * </ul>
+     */
     @Test
     public void uiAndStorage() {
         rr.then(r -> {
