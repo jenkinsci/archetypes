1c9e5907c27d2956ac54c2634f5ed1835bc9ac51
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2024-01-14 17:00:14 +0100

    Actually restart jenkins during SampleConfigurationTest

--- global-configuration/src/main/resources/archetype-resources/src/test/java/SampleConfigurationTest.java
+++ global-configuration/src/main/resources/archetype-resources/src/test/java/SampleConfigurationTest.java
@@ -1,11 +1,12 @@
-package io.jenkins.plugins.sample;
+package $package;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import org.apache.commons.io.FileUtils;
 import org.htmlunit.html.HtmlForm;
 import org.htmlunit.html.HtmlTextInput;
 import org.junit.jupiter.api.Test;
-import org.junit.runners.model.Statement;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
@@ -23,30 +24,45 @@ class SampleConfigurationTest {
      */
     @Test
     void uiAndStorage(JenkinsRule jenkins) throws Throwable {
-        jenkins.apply(
-                new Statement() {
-                    @Override
-                    public void evaluate() throws Throwable {
-                        assertNull(SampleConfiguration.get().getLabel(), "not set initially");
-                        HtmlForm config =
-                                jenkins.createWebClient().goTo("configure").getFormByName("config");
-                        HtmlTextInput textbox = config.getInputByName("_.label");
-                        textbox.setText("hello");
-                        jenkins.submit(config);
-                        assertEquals(
-                                "hello", SampleConfiguration.get().getLabel(), "global config page let us edit it");
-                    }
-                },
-                jenkins.getTestDescription());
+        assertNull(SampleConfiguration.get().getLabel(), "not set initially");
+        try (JenkinsRule.WebClient client = jenkins.createWebClient()) {
+            HtmlForm config = client.goTo("configure").getFormByName("config");
+            HtmlTextInput textbox = config.getInputByName("_.label");
+            textbox.setText("hello");
+            jenkins.submit(config);
+            assertEquals("hello", SampleConfiguration.get().getLabel(), "global config page let us edit it");
+        }
 
-        jenkins.apply(
-                new Statement() {
-                    @Override
-                    public void evaluate() throws Throwable {
-                        assertEquals(
-                                "hello", SampleConfiguration.get().getLabel(), "still there after restart of Jenkins");
-                    }
-                },
-                jenkins.getTestDescription());
+        jenkins = restartJenkins(jenkins);
+
+        assertEquals("hello", SampleConfiguration.get().getLabel(), "still there after restart of Jenkins");
+
+        FileUtils.deleteQuietly(jenkins.getInstance().getRootDir());
+    }
+
+    private static JenkinsRule restartJenkins(JenkinsRule currentJenkinsRule) throws Throwable {
+        // backup current instance
+        File backup = new File(currentJenkinsRule.getInstance().getRootDir() + "_" + System.currentTimeMillis());
+        FileUtils.copyDirectory(currentJenkinsRule.getInstance().getRootDir(), backup);
+
+        // shutdown and cleanup current instance
+        currentJenkinsRule.after();
+
+        // init new instance with previous port and home
+        JenkinsRule newJenkinsRule = new JenkinsRule() {
+            @Override
+            public void before() throws Throwable {
+                this.localPort = currentJenkinsRule.getURL().getPort();
+                this.testDescription = currentJenkinsRule.getTestDescription();
+                super.before();
+            }
+        }.withExistingHome(backup);
+
+        newJenkinsRule.before();
+
+        // delete backup
+        FileUtils.deleteQuietly(backup);
+
+        return newJenkinsRule;
     }
 }
