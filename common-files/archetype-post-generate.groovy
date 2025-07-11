983a0a3c4151a2f4204a28992b68e0853d048687
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-07-11 15:45:49 +0100

    fix theme names that contain a '-' (#822)
    Signed-off-by: Lewis Birks <22620804+lewisbirks@users.noreply.github.com>

--- common-files/archetype-post-generate.groovy
+++ common-files/archetype-post-generate.groovy
@@ -1,6 +1,7 @@
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Stream
 
 Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
 Properties properties = request.properties
@@ -20,6 +21,40 @@ if (properties.get("hostOnJenkinsGitHub") == "false") {
     }
     directoriesToRemove.each {
         projectPath.resolve(it).toFile().deleteDir()
+    }
+}
+
+if (properties.get('theme') != null) {
+    String theme = properties.get('theme')
+    def parts = theme.split("-").collect {it.capitalize()}
+    String themeClassName = parts.join('')
+    String themeTitleName = parts.join(' ')
+
+    replaceInFile('__themeClassNameRootAction.java', (themeClassName + 'RootAction.java') as String, themeClassName, themeTitleName, projectPath)
+    replaceInFile('__themeClassNameTheme.java',( themeClassName + 'Theme.java') as String, themeClassName, themeTitleName, projectPath)
+    replaceInFile('index.jelly', null, themeClassName, themeTitleName, projectPath)
+    replaceInFile('pom.xml', null, themeClassName, themeTitleName, projectPath)
+    replaceInFile('README.md', null, themeClassName, themeTitleName, projectPath)
+}
+
+// Needs to be defined outside the if block to avoid compilation error
+static def replaceInFile(String old, String replacement, String themeClassName, String themeTitleName, Path projectPath) {
+    try (Stream<Path> pathStream = Files.find(
+            projectPath,
+            Integer.MAX_VALUE,
+            (path, attr) -> path.getFileName().toString() == old
+    )) {
+        pathStream.findFirst()
+                .ifPresent {
+                    def file = it.toFile()
+                    String content = file.text
+                    content = content.replace('$THEME_CLASS_NAME', themeClassName)
+                    content = content.replace('$THEME_TITLE_NAME', themeTitleName)
+                    file.text = content
+                    if (replacement != null) {
+                        Files.move(it, it.parent.resolve(replacement))
+                    }
+                }
     }
 }
 
